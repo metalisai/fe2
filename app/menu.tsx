40cd87c60655aefe598a9b6eb5d75906c9cb0720
Talis Tammearu <talis995@gmail.com>
2023-11-17 20:12:25 +0200

    done?

---
+++ app/menu.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import Image from 'next/image'
+import styles from './page.module.css'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import React, { useState, useEffect } from 'react';
+
+export default function Menu() {
+  const [isMediaQueryMatched, setIsMediaQueryMatched] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuClick = () => {
+    const nav = document.querySelector(`.${styles.navMobile}`);
+    const menuBtn = document.querySelector('button');
+    nav.classList.toggle(styles.navShowMobile);
+    setIsMenuOpen(!isMenuOpen);
+    console.log('menu clicked ' + isMenuOpen);
+  }
+
+  useEffect(() => {
+    // Define the media query
+    const mediaQuery = window.matchMedia('(max-width: 700px)');
+
+    // Initial check for the media query
+    setIsMediaQueryMatched(mediaQuery.matches);
+
+
+    // Add a listener to update the state when the media query changes
+    const handleMediaQueryChange = (event) => {
+      console.log('media query changed' + isMenuOpen);
+      if (event.matches) {
+        console.log('menu open when media query changed');
+        setIsMenuOpen(false);
+      }
+      setIsMediaQueryMatched(event.matches);
+    };
+
+    mediaQuery.addListener(handleMediaQueryChange);
+
+    // Clean up the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <nav>
+        {isMediaQueryMatched ? (
+          <div>
+            {
+            !isMenuOpen ? (
+                <button onClick={menuClick} className={styles.menuBtn}><Image src="./icon-menu.svg" alt="menu" width={24} height={18} /></button>
+              ) : (
+                <button onClick={menuClick} className={styles.menuBtn}><Image src="./icon-menu-close.svg" alt="menu" width={24} height={18} /></button>
+              )
+            }
+            <div className={styles.navMobile}>
+              <div className={styles.navMobileOverlay}>
+                <div className={styles.dimmer}></div>
+                <ul>
+                  <li><a href="#home">Home</a></li>
+                  <li><a href="#new">New</a></li>
+                  <li><a href="#popular">Popular</a></li>
+                  <li><a href="#trending">Trending</a></li>
+                  <li><a href="#categories">Categories</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>) : (
+          <ul className={styles.navDesktop}>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#new">New</a></li>
+            <li><a href="#popular">Popular</a></li>
+            <li><a href="#trending">Trending</a></li>
+            <li><a href="#categories">Categories</a></li>
+          </ul>)
+        }
+    </nav>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,81 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Menu from './menu'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.logoCont}>
+          <Image src="./logo.svg" alt="logo" width={128} height={128} />
         </div>
+        <Menu />
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <main className={styles.main}>
+        <div className={styles.newsBlock}>
+          <div className={styles.highlightArticle}>
+            <div className={styles.highlightImage}>
+              <Image src="/image-web-3-desktop.jpg" alt="highlight image" width={1460} height={600} sizes="100vw" style={styles.mainimg} layout="responsive" />
+            </div>
+            <div className={styles.highlightText}>
+              <h1>The Bright Future of Web 3.0?</h1>
+              <div className={styles.highlightDesc}>
+                <p>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</p>
+                <button>Read More</button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.newArticles}>
+            <h2>New</h2>
+            <div className={styles.newArticle}>
+              <h3>Hydrogen VS Electric Cars</h3>
+              <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+            </div>
+            <hr />
+            <div className={styles.newArticle}>
+              <h3>The Downsides of AI Artistry</h3>
+              <p>What are the possible adverse effects of on-demand AI image generation?</p>
+            </div>
+            <hr />
+            <div className={styles.newArticle}>
+              <h3>Is VC Funding Drying Up?</h3>
+              <p>Private funding VC firms is down 50% YOY. We take a look at what that means.</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.popularBlock}>
+          <div className={styles.popularItem}>
+            <div className={styles.popularImage}>
+              <Image src="/image-retro-pcs.jpg" alt="retro pc" width={200} height={254} sizes="100vw" />
+            </div>
+            <div className={styles.popularText}>
+              <div className={styles.popularNr}>01</div>
+              <h3>Reviving Retro PCs</h3>
+              <p>What happens when old PCs are given modern upgrades?</p>
+            </div>
+          </div>
+          <div className={styles.popularItem}>
+            <div className={styles.popularImage}>
+              <Image src="/image-top-laptops.jpg" alt="top laptops" width={200} height={254} sizes="100vw"/>
+            </div>
+            <div className={styles.popularText}>
+              <div className={styles.popularNr}>02</div>
+              <h3>Top 10 Laptops of 2022</h3>
+              <p>Our best picks for various needs and budgets.</p>
+            </div>
+          </div>
+          <div className={styles.popularItem}>
+            <div className={styles.popularImage}>
+              <Image src="/image-gaming-growth.jpg" alt="gaming growth" width={200} height={254} sizes="100vw"/>
+            </div>
+            <div className={styles.popularText}>
+              <div className={styles.popularNr}>03</div>
+              <h3>The growth of gaming</h3>
+              <p>How the pandemic has sparked fresh opportunities.</p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
   )
 }
